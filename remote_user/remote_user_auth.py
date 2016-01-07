22910cb1e2c07edf77012bac5de4df2b527f2ffb
Ben Butchart <b.butchart@ed.ac.uk>
2016-01-07 09:28:38 +0000

    extend LocalAuthenticator and implement authenticate method to add a system user as side effect of login

--- remote_user/remote_user_auth.py
+++ remote_user/remote_user_auth.py
@@ -1,40 +1,96 @@
-
 import os
+
 from jupyterhub.handlers import BaseHandler
+
 from jupyterhub.auth import Authenticator
+
+from jupyterhub.auth import LocalAuthenticator
+
 from jupyterhub.utils import url_path_join
+
 from tornado import gen, web
+
 from traitlets import Unicode
+
+
+
 
 
 class RemoteUserLoginHandler(BaseHandler):
 
-    def get(self):
-        header_name = self.authenticator.header_name
-        remote_user = self.request.headers.get(header_name, "")
-        if remote_user == "":
-            raise web.HTTPError(401)
-        else:
-            user = self.user_from_username(remote_user)
-            self.set_login_cookie(user)
-            self.redirect(url_path_join(self.hub.server.base_url, 'home'))
 
-
-class RemoteUserAuthenticator(Authenticator):
-    """
-    Accept the authenticated user name from the REMOTE_USER HTTP header.
-    """
-    header_name = Unicode(
-        default_value='REMOTE_USER',
-        config=True,
-        help="""HTTP header to inspect for the authenticated username.""")
-
-    def get_handlers(self, app):
-        return [
-            (r'/login', RemoteUserLoginHandler),
-        ]
 
     @gen.coroutine
-    def authenticate(self, *args):
-        raise NotImplementedError()
+
+    def get(self):
+
+
+        header_name = self.authenticator.header_name
+
+        remote_user = self.request.headers.get(header_name, "")
+
+        data = {'username': remote_user }
+
+        username = yield self.authenticator.authenticate(self,data)
+
+        if username:
+
+            user = self.user_from_username(username)
+
+            self.set_login_cookie(user)
+
+            self.redirect(url_path_join(self.hub.server.base_url, 'home'))
+
+        else:
+
+
+            raise web.HTTPError(403)
+
+
+
+
+
+class RemoteUserAuthenticator(LocalAuthenticator):
+
+    """
+
+    Accept the authenticated user name from the REMOTE_USER HTTP header.
+
+    """
+
+    header_name = Unicode(
+
+        default_value='REMOTE_USER',
+
+        config=True,
+
+        help="""HTTP header to inspect for the authenticated username.""")
+
+
+
+    def get_handlers(self, app):
+
+        return [
+
+            (r'/login', RemoteUserLoginHandler),
+
+        ]
+
+
+
+    @gen.coroutine
+
+    def authenticate(self, handler, data):
+
+        username = data['username']
+
+        nameduser = handler.user_from_username(username)
+
+        self.add_user(nameduser)
+
+        return username
+
+
+
+
 
