d4285f6a078bdcdc58881212ceba1effebe7c1a2
Gavin <gavingray1729@gmail.com>
2016-01-09 17:09:42 +0000

    mistake

--- remote_user/remote_user_auth.py
+++ remote_user/remote_user_auth.py
@@ -97,8 +97,8 @@ class RemoteUserAuthenticator(LocalAuthenticator):
 
         self.add_user(nameduser)
 
-        if not os.path.exists(os.path.join(["/home", nameduser.name, 
-                                            "dds-notebooks"]))
+        if not os.path.exists(os.path.join("/home", nameduser.name,
+                                           "dds-notebooks"))
             # if we have a user initialisation script, run it now
             if self.postadduser_script:
                 subprocess.call([self.postadduser_script, nameduser.name])
